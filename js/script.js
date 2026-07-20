a7d0fa5705567ff4837f5897df0c7ea5e4825bf0
yaseenclinic-cbct <1yaseenct1@gmail.com>
2026-07-20 16:55:38 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,66 @@
+// =========================
+// YRMS v0.1
+// =========================
 
+// Temporary doctor information
+// Later this data will come from Google Sheets
+
+const doctor = {
+    name: "جاري تحميل البيانات...",
+    clinic: "..."
+};
+
+document.getElementById("doctorName").textContent = doctor.name;
+document.getElementById("clinicName").textContent = doctor.clinic;
+
+
+// X-Ray Types
+
+const xrayTypes = [
+    "CBCT Tooth",
+    "CBCT Jaw",
+    "CBCT Full Mouth",
+    "OPG",
+    "Cephalometric",
+    "3D Proface",
+    "Surgical Guide",
+    "Report",
+    "Ceph Analysis"
+];
+
+const container = document.getElementById("xrayContainer");
+
+xrayTypes.forEach(type => {
+
+    container.innerHTML += 
+        <div class="form-check xray-item">
+
+            <input
+                class="form-check-input"
+                type="checkbox"
+                value="${type}"
+                id="${type}">
+
+            <label
+                class="form-check-label"
+                for="${type}">
+
+                ${type}
+
+            </label>
+
+        </div>
+    ;
+
+});
+
+
+// Submit Button
+
+document
+.getElementById("submitBtn")
+.addEventListener("click", function(){
+
+    alert("هذه النسخة التجريبية فقط.");
+
+});
