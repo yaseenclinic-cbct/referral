97461c937aedb8f4cb0d72993e634a052f206f1e
yaseenclinic-cbct <1yaseenct1@gmail.com>
2026-07-22 19:58:55 +0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,20 @@
+import { db } from "./firebase.js";
 
+import {
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+async function loadDashboard() {
+
+    const doctors = await getDocs(collection(db, "doctors"));
+    const clinics = await getDocs(collection(db, "clinics"));
+    const referrals = await getDocs(collection(db, "referrals"));
+
+    document.getElementById("doctorCount").textContent = doctors.size;
+    document.getElementById("clinicCount").textContent = clinics.size;
+    document.getElementById("referralCount").textContent = referrals.size;
+
+}
+
+loadDashboard();
