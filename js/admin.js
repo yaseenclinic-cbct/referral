542c931004fe7f6cd1ef2d87b9d882767fe3a098
yaseenclinic-cbct <1yaseenct1@gmail.com>
2026-07-22 20:08:29 +0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -2,7 +2,9 @@ import { db } from "./firebase.js";
 
 import {
     collection,
-    getDocs
+    getDocs,
+    doc,
+    setDoc
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 async function loadDashboard() {
@@ -18,3 +20,54 @@ async function loadDashboard() {
 }
 
 loadDashboard();
+
+const clinicModal =
+new bootstrap.Modal(document.getElementById("clinicModal"));
+
+document
+.getElementById("addClinicBtn")
+.addEventListener("click", () => {
+
+    clinicModal.show();
+
+});
+
+document
+.getElementById("saveClinicBtn")
+.addEventListener("click", async () => {
+
+    const code =
+    document.getElementById("clinicCode")
+    .value
+    .trim()
+    .toUpperCase();
+
+    const name =
+    document.getElementById("clinicName")
+    .value
+    .trim();
+
+    if (!code || !name) {
+
+        alert("املأ جميع الحقول");
+
+        return;
+
+    }
+
+    await setDoc(doc(db, "clinics", code), {
+
+        code: code,
+        name: name,
+        active: true,
+        createdAt: new Date()
+
+    });
+
+    clinicModal.hide();
+
+    alert("تمت إضافة العيادة بنجاح");
+
+    loadDashboard();
+
+});
