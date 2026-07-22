db8f5d63bcd612fe7b49db1cb2902d5e82efca70
yaseenclinic-cbct <1yaseenct1@gmail.com>
2026-07-22 16:30:40 +0300

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD_UlJ2BXnqp1KEuDthunrPJDp3VUsgRBM",
+  authDomain: "yrms-c2d60.firebaseapp.com",
+  projectId: "yrms-c2d60",
+  storageBucket: "yrms-c2d60.firebasestorage.app",
+  messagingSenderId: "929472304920",
+  appId: "1:929472304920:web:e1e87a2cb559163d79b06d"
+};
